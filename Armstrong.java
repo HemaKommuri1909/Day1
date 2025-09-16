53b0e5db53eb7dfe968832e8c8858a8f39ac1736
Hema <152959936+HemaKommuri1909@users.noreply.github.com>
2025-09-16 22:05:54 +0530

    Control_Statments

--- Armstrong.java
+++ Armstrong.java
@@ -14,16 +14,16 @@ public class Armstrong {
 			temp/=10;
 			count++;
 			}
-		temp=num;
-		while(temp!=0) {
-			int r=temp%10;
-			//arm+=Math.pow(r,count);
-			int pow=1;
-			for(int i=0;i<count;i++) {
-				pow*=r;
-			}
-			arm+=pow;
-			temp/=10;	
+		//temp=num;
+		while(num!=0) {
+			int r=num%10;
+			arm+=Math.pow(r,count);
+//			int pow=1;
+//			for(int i=0;i<count;i++) {
+//				pow*=r;
+//			}
+//			arm+=pow;
+			num/=10;	
 			}
         System.out.println(arm);
         if(arm==original) {
